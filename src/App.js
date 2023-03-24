87a8a2df2055646109003a73a12e7e2f81969dbd
Tetyana123 <oookbtatyana@gmail.com>
2023-03-24 17:56:21 +0200

    add html +body

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+// import background from './images/album.jpg';
+// import Button from 'react-bootstrap/Button';
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
+    // <Container fluid="md" >
+    // <Row>
+    // <Col>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
+      {/* <div  style={{ backgroundImage:`url(${background})` }} className="Background">   */}
+        
+        <h1 className="Headline">
+          Create your <br/> personalised <br/> photobook
+        </h1>
+        
+        <h1 className="Subtitle">
+          Here you can breathe new life <br/> into your favorite photos
+        </h1>
+
+        {/* <Button variant="primary" size="lg"
+        >Start creating</Button>{' '} */}
+         <div className="Link">
+           <a
+          className="Button-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Start creating
         </a>
-      </header>
-    </div>
+        </div>
+      </div>
+    // </div>
+    // </Col> 
+    // </Row>
+    // </Container>
   );
 }
 
